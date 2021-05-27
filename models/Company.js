229a9ee1ab546d3895af1b9c3ad60cd4dfb30520
chaitalizn <phchaitali3@gmail.com>
2021-05-27 08:24:02 -0400

    created company model

---
+++ models/Company.js
@@ -0,0 +1,80 @@
+//require sequelize
+const { Model, DataTypes } = require('sequelize');
+//connect to sequelize
+const sequelize = require('../config/connection');
+
+//create company model
+class Company extends Model {} 
+
+//create fields/columns for Company model
+Company.init(
+    {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    company_name: {
+         type: DataTypes.STRING,
+         allowNull: false,
+    },
+    address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    phone: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    company_email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique:true,
+        validate: {
+            isEmail: true
+        }
+    },
+    website: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        validate: {
+            isURL: true
+        }
+    },
+    about: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+    },
+    industry_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'industry',
+            key: 'id'
+          }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+          }
+    },
+    products_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'products',
+            key: 'id'
+          }
+    }
+    //create a hours model and add references
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'company'
+      }
+);
+
+module.exports = Company;
