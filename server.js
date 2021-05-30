cb536123daaadcd0c2214e8df7f410e92bd6674a
Solidshots <seanbrent5@gmail.com>
2021-05-30 19:14:05 -0400

    session init in server

--- server.js
+++ server.js
@@ -2,10 +2,25 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 //Initialize express app and define port for server to use
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//define the session configuration and cookie settings for the session
+const sess = {
+    secret: 'An amazing secret for nobody to see or use',
+    cookie: { maxAge: 60000 * 10 },
+    rolling: true,
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({db: sequelize})
+}
+
+//Server is instructed to use session data
+app.use(session(sess));
 
 //Express middleware to accept incoming json objects
 app.use(express.json());
