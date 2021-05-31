076975526bd31a5561e2655a3817eb3f3898c6a2
Solidshots <seanbrent5@gmail.com>
2021-05-31 07:18:53 -0400

    Preparing for handlebars

--- models/index.js
+++ models/index.js
@@ -10,8 +10,6 @@ User.hasMany(Company, {foreignKey: 'user_id'});
 //A company must belong to a single user
 Company.belongsTo(User, {foreignKey: 'user_id'});
 
-//I believe it should look more like this:
-
 //A company has only a single set of weekly hours
 Company.hasOne(Hours, {foreignKey: 'company_id'});
 //A set of hours can only belong to a single company 
@@ -26,29 +24,6 @@ Company.belongsTo(Industry, {foreignKey: 'industry_id'});
 Product.belongsTo(Company, {foreignKey: 'company_id'});
 //A product has to belong to a single company
 Company.hasMany(Product, {foreignKey: 'company_id'});
-
-//Few notes here: 
-//1. products, hours, and industry should belong to the company and not the user. User is tied to the company and the rest of the models are tied to the company.
-//2. onDelete is set to SET NULL by default, no need to define
-
-// Products.belongsTo(User, {
-//     foreignKey: 'user_id',
-//     onDelete: 'SET NULL'
-//   });
-
-// Hours.belongsTo(User, {
-//     foreignKey: 'user_id',
-//     onDelete: 'SET NULL'
-// });
-
-// Industry.belongsToMany(User, {
-//   foreignKey: 'user_id',
-//   onDelete: 'SET NULL'
-// });
-
-// User.hasMany(Products, {
-//   foreignKey: 'user_id'
-// });
 
 
 module.exports = { User, Company, Industry, Hours, Product };
