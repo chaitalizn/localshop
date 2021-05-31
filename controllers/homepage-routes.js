9b534d21331167f6c2e5dd6b044efb6ffe21aab6
chaitalizn <phchaitali3@gmail.com>
2021-05-31 12:17:52 -0400

    added html and handlebar for result page

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -14,4 +14,17 @@ router.get('/login', (req, res) => {
     res.render('login');
 })
 
+//Result Page Route
+router.get('/result/:id', (req, res) => {
+    //hardcoded data for testing ONLY
+    const company = {
+        id: 1,
+        company_name: 'test_name',
+        about: 'info about thie fake company',
+        phone: '4561231236'
+      };
+
+    res.render('result');
+})
+
 module.exports = router;
