a66b085d84a1fe6538f0a058680c6aeb661e34f0
chaitalizn <phchaitali3@gmail.com>
2021-05-31 20:43:53 -0400

    updated dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -4,6 +4,16 @@ const { Company, Hours, Industry, Product, User } = require('../models');
 
 //GET route for the main page
 router.get('/', (req, res) => {
+    //hardcoded data for testing ONLY
+    const company = {
+        company_name: "test inc",
+        address: '2345 street lane',
+        phone: 9058524562,
+        company_email: "sales@test.com",
+        website: "www.test.com",
+        about_us: "this is a test company. we are invisiable",
+        industry: "fake"
+      };
     res.render('dashboard', { loggedIn: true });
   });
 
