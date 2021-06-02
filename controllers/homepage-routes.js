5091fcfd1b978b3a56bf187332c7305e86457d4e
Solidshots <seanbrent5@gmail.com>
2021-06-01 23:12:30 -0400

    search functionality working

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -37,19 +37,6 @@ router.get('/company/:id', (req, res) => {
         });
 });
 
-//Result Page Route
-router.get('/result/:id', (req, res) => {
-    //hardcoded data for testing ONLY
-    const company = {
-        id: 1,
-        company_name: 'test_name',
-        about: 'info about thie fake company',
-        phone: '4561231236'
-      };
-
-    res.render('result');
-})
-
 router.get('/searchAll', (req, res) => {
     Company.findAll({})
     .then(dbCompanyData => {
