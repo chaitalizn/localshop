6a60aaea8214598cf92f42d4f142d9cfc20c22d1
chaitalizn <phchaitali3@gmail.com>
2021-06-06 14:27:35 -0400

    refactor sidebar code

--- public/javascript/sidebar.js
+++ public/javascript/sidebar.js
@@ -1,9 +1,20 @@
-/* Set the width of the sidebar to 250px (show it) */
+/* Set the width of the sidebar to 250px (show it)
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
   }
   
-  /* Set the width of the sidebar to 0 (hide it) */
+  /* Set the width of the sidebar to 0 (hide it)
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
   }
+  */
+
+  function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
